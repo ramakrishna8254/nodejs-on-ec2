e479888cf8b375aa33ebb1397ef2758d0b46f6b9
ramakrishna8254 <109083683+ramakrishna8254@users.noreply.github.com>
2023-02-20 19:03:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Cloud4C Technologies Hyderabad'); //write a response to the client
+  res.write('Cloud4C Technologies Hyderabad.........AWS DEVOPS WORKING PARTNER'); //write a response to the client
   res.end(); //end the response
 }).listen(9981); //the server object listens on port 80
