cfe6c3ca4d82e2693aebce828012d85e64fdc06e
ramakrishna8254 <109083683+ramakrishna8254@users.noreply.github.com>
2023-02-20 19:13:00 +0530

    Create sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,20 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://ec2-3-108-236-125.ap-south-1.compute.amazonaws.com:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'SampleNodeJSApplication',
+	    'sonar.projectKey':'samplenodejsapplication',
+	    'sonar.login': 'admin',
+	    'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	    //'sonar.exclusions': '**/node_modules/**,/coverage/lcov-report/*.test/*.js',
+	    //'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+	    //'sonar.tests': 'specs',
+            //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
