fb526438d1ca574f139552bbf59815bb060fabd8
ramakrishna8254 <109083683+ramakrishna8254@users.noreply.github.com>
2023-02-20 22:34:25 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://ec2-3-108-236-125.ap-south-1.compute.amazonaws.com:9000/',
+  serverUrl: 'http://ec2-13-233-29-207.ap-south-1.compute.amazonaws.com:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'SampleNodeJSApplication',
